173fb0aa10e820654fdf8b404fddf5883b17a2ce
Alex Peaty <alexpeaty1@gmail.com>
2024-08-24 14:30:48 +0100

    Added a button to alter the number of squares in the grid

--- script.js
+++ script.js
@@ -2,27 +2,49 @@ const container = document.getElementById('grid-container');
    
 container.style.display = 'flex';
 container.style.flexWrap = 'wrap';
-container.style.width = '800px'; // Set a fixed width for the grid
-container.style.height = '800px'; // Set a fixed height for the grid
+container.style.width = '700px'; // Set a fixed width for the grid
+container.style.height = '700px'; // Set a fixed height for the grid
 container.style.gap = '5px';
 
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
+function changeSizeofGrid(sizeOfGrid) {
+    const gridSize = sizeOfGrid;
+    const totalSquares = gridSize * gridSize;
 
-// Size of each square as a percent
-const squareSize = `calc(${100 / gridSize}% - 5px)`;
+    const squareSize = `calc(${100 / gridSize}% - 5px)`;
 
-for (let i = 0; i < totalSquares; i++) {
-    const div = document.createElement('div');
-    div.style.width = squareSize;
-    div.style.height = squareSize;
-    div.style.backgroundColor = 'lightblue';
-    container.appendChild(div);
-    div.addEventListener('mouseover', function() {
-        div.style.backgroundColor = 'blue';
-        setTimeout(function() {
-            div.style.backgroundColor = 'lightblue';
-        }, 500)
+    for (let i = 0; i < totalSquares; i++) {
+        const div = document.createElement('div');
+        div.style.width = squareSize;
+        div.style.height = squareSize;
+        div.style.backgroundColor = 'lightblue';
+        container.appendChild(div);
+        div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = 'blue';
+            setTimeout(function() {
+                div.style.backgroundColor = 'lightblue';
+            }, 500)
+        })
+    }
+
+    const btn = document.createElement('button');
+    btn.style.color = 'black';
+    btn.style.borderRadius = '5px';
+    btn.style.borderColor = 'black'
+    btn.style.width = '200px';
+    btn.style.height = '100px';
+
+    btn.addEventListener('click', function() {
+        let size = parseInt(prompt('Enter grid squares:'));
+        if (size <=0 || size > 100) {
+            alert('Number of squares is out of boundaries');
+        } else {
+            container.innerHTML = '';
+            changeSizeofGrid(size);
+        }
     })
-    
+
+    container.appendChild(btn);
 }
+
+changeSizeofGrid(16);
+
